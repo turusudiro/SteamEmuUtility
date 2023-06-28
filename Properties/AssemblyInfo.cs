e869c5d6cd5a91b8527c5a0c19fa3945d7180ea6
turusudiro <nimanyosafat25@gmail.com>
2023-06-28 14:43:15 +0700

    Changed Properties name from GreenBerg to SteamEmuUtility, deleted unused xml file.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GreenBerg")]
+[assembly: AssemblyTitle("SteamEmuUtility")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GreenBerg")]
+[assembly: AssemblyProduct("SteamEmuUtility")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
