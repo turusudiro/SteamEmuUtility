8d99388856a3886194358440a739516290d6aef7
turusudiro <nimanyosafat25@gmail.com>
2023-09-14 23:31:10 +0700

    - Removed GL Features - Added individual DLC, Game, Full Features

--- Common/GreenLumaCommon.cs
+++ Common/GreenLumaCommon.cs
@@ -1,20 +1,69 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Playnite.SDK;
+using Playnite.SDK.Events;
 using Playnite.SDK.Models;
+using SteamEmuUtility.Services;
 
 namespace SteamEmuUtility.Common
 {
     public class GreenLumaCommon
     {
-        private const string greenlumafeature = "Steam Emu Utility : [GL] Enabled";
+        private const string greenlumagame = "Steam Emu Utility : [GL] Game Unlocking";
+        private const string greenlumadlc = "Steam Emu Utility : [GL] DLC Unlocking";
+        private const string greenlumafull = "Steam Emu Utility : [GL] Game And DLC Unlocking";
         public const string user32 = "User32.dll";
         private const string stealth = "NoQuestion.bin";
 
-        public static GameFeature GreenLumaFeature(IPlayniteAPI PlayniteApi)
+        public static Task InjectGame(OnGameStartingEventArgs args, SteamEmuUtility plugin, ILogger logger)
         {
-            return PlayniteApi.Database.Features.Add(greenlumafeature);
+
+            WriteAppList(args.Game.GameId, 0);
+            return Task.CompletedTask;
+        }
+        public static Task InjectDLC(OnGameStartingEventArgs args, SteamEmuUtility plugin, ILogger logger, int count)
+        {
+            string dlcpath = $"{plugin.GetPluginUserDataPath()}\\Common\\{args.Game.GameId}.txt";
+            if (File.Exists(dlcpath))
+            {
+                foreach (string line in File.ReadLines(dlcpath))
+                {
+                    WriteAppList(line, count);
+                    count++;
+                }
+                return Task.CompletedTask;
+            }
+            else
+            {
+                var Steamservice = new SteamService(plugin, logger);
+                GlobalProgressOptions progress = new GlobalProgressOptions("Steam Emu Utility");
+                var b = plugin.PlayniteApi.Dialogs.ActivateGlobalProgress((global) =>
+                {
+                    global.IsIndeterminate = false;
+                    global.ProgressMaxValue = 1;
+                    global.CurrentProgressValue = 0;
+                    logger.Info("Getting DLC Info...");
+                    global.Text = $"Getting DLC Info for {args.Game.Name}";
+                    global.CurrentProgressValue++;
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    Task.Run(() => Steamservice.GetDLCStore(args.Game, global));
+                }, progress);
+                return Task.CompletedTask;
+            }
+        }
+        public static GameFeature GreenLumaFull(IPlayniteAPI PlayniteApi)
+        {
+            return PlayniteApi.Database.Features.Add(greenlumafull);
+        }
+        public static GameFeature GreenLumaGame(IPlayniteAPI PlayniteApi)
+        {
+            return PlayniteApi.Database.Features.Add(greenlumagame);
+        }
+        public static GameFeature GreenLumaDLC(IPlayniteAPI PlayniteApi)
+        {
+            return PlayniteApi.Database.Features.Add(greenlumadlc);
         }
         public static Task InjectUser32(string dll)
         {
@@ -40,9 +89,8 @@ namespace SteamEmuUtility.Common
                 return Task.FromException(ex);
             }
         }
-        public static Task WriteAppList(string appid)
+        public static Task WriteAppList(string appid, int count)
         {
-            int count = 0;
             if (!Directory.Exists(Path.Combine(SteamCommon.GetSteamDir(), "applist")))
             {
                 Directory.CreateDirectory(Path.Combine(Path.Combine(SteamCommon.GetSteamDir(), "applist")));
