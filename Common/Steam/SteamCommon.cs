25e7e4e824c6d7fbc3366c431bf7426e43be29fe
turusudiro <nimanyosafat25@gmail.com>
2024-07-03 06:00:57 +0700

    added some simple method related to steam

--- Common/Steam/SteamCommon.cs
+++ Common/Steam/SteamCommon.cs
@@ -13,19 +13,11 @@ namespace SteamCommon
         private static Guid steamPluginId = Guid.Parse("cb91dfc9-b977-43bf-8e70-55f46e410fab");
         private const string defaultexe = @"C:\Program Files (x86)\Steam\steam.exe";
         private const string defaultdir = @"C:\Program Files (x86)\Steam";
-        private static string GetSteamDir()
-        {
-            using (var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Valve\Steam"))
-            {
-                if (key?.GetValueNames().Contains("SteamPath") == true)
-                {
-                    return key.GetValue("SteamPath")?.ToString().Replace('/', '\\') ?? defaultdir;
-                }
-            }
-
-            return defaultdir;
-        }
-        private static string GetSteamExe()
+        /// <summary>
+        /// Get Steam executable path.
+        /// </summary>
+        /// <returns> Steam executable path</returns>
+        public static string GetSteamExecutable()
         {
             using (var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Valve\Steam"))
             {
@@ -33,30 +25,28 @@ namespace SteamCommon
                 {
                     return key.GetValue("SteamExe")?.ToString().Replace('/', '\\') ?? defaultexe;
                 }
-            }
-
-            return defaultexe;
-        }
-        /// <summary>
-        /// Get Steam executable path.
-        /// </summary>
-        /// <returns> Steam executable path</returns>
-        public static string SteamExecutable
-        {
-            get
-            {
-                return GetSteamExe();
+                else
+                {
+                    return defaultexe;
+                }
             }
         }
         /// <summary>
         /// Get Steam directory path.
         /// </summary>
         /// <returns> Steam directory path</returns>
-        public static string SteamDirectory
+        public static string GetSteamDirectory()
         {
-            get
+            using (var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Valve\Steam"))
             {
-                return GetSteamDir();
+                if (key?.GetValueNames().Contains("SteamPath") == true)
+                {
+                    return key.GetValue("SteamPath")?.ToString().Replace('/', '\\') ?? defaultdir;
+                }
+                else
+                {
+                    return defaultdir;
+                }
             }
         }
         /// <summary>
@@ -76,32 +66,48 @@ namespace SteamCommon
         /// <returns>
         /// <c>true</c> if Steam running; otherwise <c>false</c>
         /// </returns>
-        public static bool IsSteamRunning
+        public static bool IsSteamRunning()
         {
-            get
-            {
-                var steamProcesses = Process.GetProcessesByName("steam");
-                return steamProcesses.Length > 0;
-            }
+            var steamProcesses = Process.GetProcessesByName("steam");
+            return steamProcesses.Length > 0;
         }
         /// <summary>
         /// Kill Steam process.
         /// </summary>
-        /// <returns>
-        /// <c>true</c> if Steam process successfully kill; otherwise <c>false</c>
-        /// </returns>
-        public static bool KillSteam
+        public static void KillSteam()
         {
-            get
+            ProcessUtilities.ProcessKill("steam");
+        }
+        /// <summary>
+        /// Get Steam ProcessID in string.
+        /// </summary>
+        public static string GetSteamProcessId()
+        {
+            Process[] processes = Process.GetProcessesByName("steam");
+
+            foreach (Process process in processes)
             {
-                if (ProcessUtilities.ProcessKill("steam"))
+                try
                 {
-                    return true;
+                    return process.Id.ToString();
                 }
-                else
-                {
-                    return false;
-                }
+                catch { return string.Empty; }
+            }
+            return string.Empty;
+        }
+        /// <summary>
+        /// Get SteamID3 with SteamID64.
+        /// </summary>
+        /// <param name="userSteamID">The SteamID64. example: 76561197960287930</param>
+        public static ulong GetUserSteamID3(string userSteamID)
+        {
+            if (ulong.TryParse(userSteamID, out ulong userSteamID3))
+            {
+                return userSteamID3 - 76561197960265728;
+            }
+            else
+            {
+                return 0;
             }
         }
     }
