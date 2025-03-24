f85703a8d6589956af1b29d0bf1da50a8ffcf802
turusudiro <nimanyosafat25@gmail.com>
2025-03-24 18:13:59 +0700

    added appid finder models

---
+++ Common/Acf/Models/AppidFinder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace AcfGenerator.Models
+{
+    public partial class AppidFinder : ObservableObject
+    {
+        public string Name { get; set; }
+        public uint AppID { get; set; }
+        public string ImageURL { get; set; }
+        private string _selectedTarget;
+        public string SelectedTarget
+        {
+            get => _selectedTarget;
+            set
+            {
+                if (_selectedTarget != value)
+                {
+                    _selectedTarget = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
